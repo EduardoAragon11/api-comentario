a0bea89374bcd55b944659f5da07d1b460cc33a3
EduardoAragon11 <166084429+EduardoAragon11@users.noreply.github.com>
2024-11-10 11:04:04 -0500

    Update comentario.py

--- comentario.py
+++ comentario.py
@@ -1,6 +1,8 @@
 import boto3
 import uuid
 import os
+import csv
+from io import StringIO
 
 def lambda_handler(event, context):
     # Entrada (json)
@@ -13,15 +15,30 @@ def lambda_handler(event, context):
     comentario = {
         'tenant_id': tenant_id,
         'uuid': uuidv1,
-        'detalle': {
-          'texto': texto
-        }
+        'text': texto
     }
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(nombre_tabla)
     response = table.put_item(Item=comentario)
     # Salida (json)
     print(comentario)
+    
+    ## Create .csv
+    csv_buffer = StringIO()
+    csv_writer = csv.writer(csv_buffer)
+    csv_writer.writerow([comentario['tenant_id'], comentario['uuid'], comentario['text']])
+    
+    ### Upload to bucket
+    s3_client = boto3.client("s3")
+    
+    nombre_bucket = os.environ["BUCKET_NAME"]
+
+    s3_client.put_object(
+        Bucket=nombre_bucket,
+        Key=f"comentario-{comentario['uuid']}.csv",
+        Body=csv_buffer.getvalue()
+    )
+
     return {
         'statusCode': 200,
         'comentario': comentario,
